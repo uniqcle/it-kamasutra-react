ef11bb97194611c8a7c4ec6b902f335285e7ff30
Andrey <uniqcle@yandex.ru>
2025-10-10 13:42:22 +0700

    login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,31 @@
+const LoginForm = () => {
+    return (
+        <>
+            <form>
+                <div>
+                    <input placeholder={"login..."} />
+                </div>
+                <div>
+                    <input placeholder={"password..."} />
+                </div>
+                <div>
+                    <input type={"checkbox"} /> remember me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
+        </>
+    );
+};
 
 const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginForm />
+        </div>
+    );
+};
 
-	return <h1>Login component...</h1>
-}
-
-export default Login; 
+export default Login;
