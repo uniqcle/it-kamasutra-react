00b6e0da3b449a159ace2973a0c7e861e48a0e30
Andrey <uniqcle@yandex.ru>
2025-10-10 15:29:34 +0700

    login

--- src/api/users-api.js
+++ src/api/users-api.js
@@ -54,3 +54,18 @@ export const ProfileAPI = {
         });
     },
 };
+
+
+export const AuthAPI = {
+    me() {
+        return UsersAPI.authProfile();
+    },
+
+    login(email, password, rememberMe = false) {
+        return instance.post("auth/login", { email, password, rememberMe });
+    },
+
+    logout() {
+        return instance.delete("auth/login");
+    },
+};
--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,3 +1,6 @@
+import { authProfileThunkCreator } from "./profileReducer";
+import { AuthAPI } from "../api/users-api";
+
 const SET_USER_DATA = "SET_USER_DATA";
 
 const initialState = {
@@ -26,5 +29,21 @@ export const setAuthUserData = (id, email, login) => ({
     type: SET_USER_DATA,
     data: { id, email, login },
 });
+
+export const login = (email, password, rememberMe) => (dispatch) => {
+    AuthAPI.login(email, password, rememberMe).then((response) => {
+        if (response.data.resultCode === 0) {
+            dispatch(authProfileThunkCreator());
+        }
+    });
+};
+
+export const logout = () => (dispatch) => {
+    AuthAPI.logout().then((response) => {
+        if (response.data.resultCode === 0) {
+            dispatch(authProfileThunkCreator(null, null, null));
+        }
+    });
+};
 
 export default authReducer;
